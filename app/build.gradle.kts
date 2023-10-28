80b19b51f0edef83f13c8eb0b05d94bc2ccee1ff
Ramzi Jabali <ramzi@eljabali.org>
2023-10-28 14:42:21 -0700

    Added firebase dependencies successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,13 @@ android {
 }
 
 dependencies {
+    //firebase
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.4.1"))
+
+    // Add the dependency for the Realtime Database library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-database-ktx")
 
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
