5cf431899396b53d7fc040587592114bda7a4512
Ramzi Jabali <ramzi@eljabali.org>
2023-11-03 01:56:01 -0700

    Created Measurement data class

---
+++ app/src/main/java/ramzi/eljabali/firebasedatabase/tables/Measurement.kt
@@ -0,0 +1,8 @@
+package ramzi.eljabali.firebasedatabase.tables
+
+data class Measurement(
+    var title: String = "",
+    var unit: String? = null,
+    var additive: Boolean = false,
+    var creationDate: String = "" // TODO use dateformat to go back and forth from string to datedformat
+)
