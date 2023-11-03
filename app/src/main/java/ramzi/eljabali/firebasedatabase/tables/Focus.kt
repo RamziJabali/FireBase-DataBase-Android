48fbe321e7fc5da54aef9b56fb9b2a973ef73e1b
Ramzi Jabali <ramzi@eljabali.org>
2023-11-03 01:54:10 -0700

    Created data classes

---
+++ app/src/main/java/ramzi/eljabali/firebasedatabase/tables/Focus.kt
@@ -0,0 +1,6 @@
+package ramzi.eljabali.firebasedatabase.tables
+
+data class Focus(
+    var title: String = "",
+    var creationDate: String = "" // TODO use dateformat to go back and forth from string to datedformat
+)
---
+++ app/src/main/java/ramzi/eljabali/firebasedatabase/tables/User.kt
@@ -0,0 +1,8 @@
+package ramzi.eljabali.firebasedatabase.tables
+
+data class User(
+    var name: String = "",
+    var email: String? = null,
+    var hasOnBoarded: Boolean = false,
+    var creationDate: String = "" // TODO use dateformat to go back and forth from string to datedformat
+)
